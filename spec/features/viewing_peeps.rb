9d0d1061f2d9139e341aab48c6e384c54ae95451
KevinByrne <kevbee79@gmail.com>
2022-02-11 10:23:58 +0000

    viewing peeps spec updated

--- spec/features/viewing_peeps.rb
+++ spec/features/viewing_peeps.rb
@@ -1,1 +1,8 @@
-require 'peeps'
+feature 'Viewing peeps' do
+  scenario 'User can view all peeps' do
+    visit('/peeps')
+
+    expect(page).to have_content "This is a peep comment"
+    expect(page).to have_content "This is also a peep"
+  end
+end
